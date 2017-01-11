cb78dbc8de7f060a76018714063247258878a740
Alberto Varela <alberto@berriart.com>
2017-01-11 18:03:32 +0100

    Refactor Azure subnets

--- modules/datacenter/azure/main.tf
+++ modules/datacenter/azure/main.tf
@@ -15,29 +15,9 @@ resource "azurerm_resource_group" "net" {
 }
 
 # Create a virtual network in the web_servers resource group
-resource "azurerm_virtual_network" "network" {
+resource "azurerm_virtual_network" "default" {
   name                = "v-${var.alias}-net-01"
   address_space       = ["${var.net_prefix}.0.0/16"]
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.net.name}"
-
-  subnet {
-    name           = "frontend"
-    address_prefix = "${var.net_prefix}.1.0/24"
-  }
-
-  subnet {
-    name           = "backend"
-    address_prefix = "${var.net_prefix}.2.0/24"
-  }
-
-  subnet {
-    name           = "dmz"
-    address_prefix = "${var.net_prefix}.3.0/24"
-  }
-
-  subnet {
-    name           = "backend"
-    address_prefix = "${var.net_prefix}.4.0/24"
-  }
 }
